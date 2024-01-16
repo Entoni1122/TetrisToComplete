c2533bc3f30df91e4332ddfda4171ff62c698cbd
Entoni1122 <entonishkalla34@gmail.com>
2024-01-16 16:19:10 +0100

    Initial commit

---
+++ Tetris.h
@@ -0,0 +1,7 @@
+#define STAGE_WIDTH 12
+#define STAGE_HEIGHT 22
+#define TILE_SIZE 24
+#define TETROMINO_SIZE 4
+#define FONT_SIZE 40
+
+int CheckCollision(const int tetrominoStartX, const int tetrominoStartY, const int *tetromino);
